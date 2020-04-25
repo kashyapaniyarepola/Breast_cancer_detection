aa33255778f96ce71c49f674129ded223be896a4
kashyapaniyarepola <46812931+kashyapaniyarepola@users.noreply.github.com>
2020-04-25 19:43:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
+from sklearn.preprocessing import StandardScaler
 import pickle
+import pandas as pd
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb')) 
@@ -12,9 +14,16 @@ def home():
 @app.route('/predict',methods =['POST'])
 def predict():
 
-    float_features = [float(x) for x in request.from_values()]
-    final_features = [np.array(float_features)]
-    prediction = model.predict(final_features)
+    float_features = [float(x) for x in request.form.values()]
+    # final_features = [np.array(float_features)]
+    A=pd.DataFrame(float_features)
+    A=A.iloc[:].values
+    sc = StandardScaler()
+    A = sc.fit_transform(A)
+    B=[]
+    for i in A:
+        B.append(i[0])
+    prediction = model.predict([B])
 
     return render_template('index.html', prediction_text='Probability is {}'.format(prediction))
 
