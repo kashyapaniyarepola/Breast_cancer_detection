7f308bd18af11248f87ba97368e43ee1e12cd9d9
kashyapaniyarepola <46812931+kashyapaniyarepola@users.noreply.github.com>
2020-04-29 19:34:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import pandas as pd
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb')) 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
 @app.route('/predict',methods =['POST'])
 def predict():
