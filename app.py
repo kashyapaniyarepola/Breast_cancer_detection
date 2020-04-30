15bb09ab5d7344d8310445b0444c018ae20b3f58
kashyapaniyarepola <46812931+kashyapaniyarepola@users.noreply.github.com>
2020-04-30 08:30:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -20,14 +20,14 @@ def predict():
                         dict(request.form)['smoothnessWorst'] , dict(request.form)['compactnessWorst'], dict(request.form)['concavityWorst'],dict(request.form)['concavePointsWorst'] , dict(request.form)['symmetryWorst']]
     # ad = [17.99	,10.38,	122.8	,1001.0,	0.1184,	0.2776,	0.3001,	0.1471,	0.2419,	0.07871,	1.095	,0.9053,	8.589	,153.4,	0.006399,	0.04904	,0.05373,	0.01587	,0.03003,	0.006193,	25.38	,17.33,	184.6,	2019.0,	0.1622,	0.6656,	0.7119,	0.2654,	0.4601]
     float_features = [float(x) for x in final_features]
-    A=pd.DataFrame(float_features)
-    A=A.iloc[:].values
-    sc = StandardScaler()
-    A = sc.fit_transform(A)
-    B=[]
-    for i in A:
-        B.append(i[0])
-    prediction = model.predict([B])
+    # A=pd.DataFrame(float_features)
+    # A=A.iloc[:].values
+    # sc = StandardScaler()
+    # A = sc.fit_transform(A)
+    # B=[]
+    # for i in A:
+    #     B.append(i[0])
+    # prediction = model.predict([B])
 
     return jsonify({"response" : float_features[1]})
     #return render_template('index.html', prediction_text='Probability is {}'.format(prediction))
